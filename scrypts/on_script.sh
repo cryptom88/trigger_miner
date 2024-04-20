7952ad57f73f6682073c9b295e8237f940b6b543
cryptom88 <167556536+cryptom88@users.noreply.github.com>
2024-04-20 17:00:30 +0200

    Update on_script.sh

--- scrypts/on_script.sh
+++ scrypts/on_script.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 MINER_PATH="/root/xelis/xelis-blockchain/target/release/xelis_miner"
-MINER_ADDRESS="xet:rc7nactk8xfh5xyatqnh6cea2l33rrxc9k703xj02pek9r4c4e3sqmxlghh"
-DAEMON_ADDRESS="161.97.169.235:8080"
+#MINER_ADDRESS="xet:rc7nactk8xfh5xyatqnh6cea2l33rrxc9k703xj02pek9r4c4e3sqmxlghh"
+MINER_ADDRESS="xel:npd2egtvcx4v6lwtshm6ytpglwgn0sd7d9lm6q9x5k92hlzjpvusq0uxwd7"
+#DAEMON_ADDRESS="161.97.169.235:8080"
+DAEMON_ADDRESS="5.199.161.146:8080"
 NUM_THREADS_FILE="/root/xelis/trigger_miner/scrypts/threads_count"
 
 # Retrieve the hostname and append the prefix 'mach_'
@@ -16,8 +18,8 @@ else
 fi
 
 # Construct the command using NUM_THREADS if it is not empty
-#MINER_CMD="$MINER_PATH --miner-address $MINER_ADDRESS --worker $WORKER_NAME $( [ -n "$NUM_THREADS" ] && echo "--num-threads $NUM_THREADS" ) --daemon-address $DAEMON_ADDRESS"
-MINER_CMD="$MINER_PATH --miner-address $MINER_ADDRESS --worker $WORKER_NAME --num-threads 2 --daemon-address $DAEMON_ADDRESS"
+MINER_CMD="$MINER_PATH --miner-address $MINER_ADDRESS --worker $WORKER_NAME $( [ -n "$NUM_THREADS" ] && echo "--num-threads $NUM_THREADS" ) --daemon-address $DAEMON_ADDRESS"
+#MINER_CMD="$MINER_PATH --miner-address $MINER_ADDRESS --worker $WORKER_NAME --num-threads 2 --daemon-address $DAEMON_ADDRESS"
 
 # Execute the miner command if the miner is not already running
 if ! pgrep -f "xelis_miner" > /dev/null; then
